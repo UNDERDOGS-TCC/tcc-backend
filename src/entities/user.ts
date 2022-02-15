17aa319fa066b35bfa8e58544f1338f0d86fe116
ouwargui <guikemon2@gmail.com>
2022-02-15 11:24:33 -0300

    chore: remove unused code

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -4,9 +4,5 @@ export class User {
     public email: string,
     public password: string,
     public readonly id?: string,
-    ) {
-    if (!id) {
-      this.id = Math.random().toString(36);
-    }
-  }
+    ) {}
 }
